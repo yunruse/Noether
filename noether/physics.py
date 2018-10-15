95d8013304f4c521ccbb642c83a0c0bac7327bd3
Jack Neil Dobson <jackndobson@gmail.com>
2018-10-15 17:07:08 +0100

    Add useful maths functions

--- noether/physics.py
+++ noether/physics.py
@@ -2,11 +2,14 @@
 
 import math
 
-from math import log, exp, floor, ceil, radians as rad, degrees as deg
+from math import (
+    log, exp, floor, ceil,
+    radians as rad, degrees as deg
+)
 
 from .trigonometry import rad, deg, TrigonometryState as _trigState
 
-from .helpers import clear
+from .helpers import clear, intify
 from .constants import *
 
 from . import units
@@ -19,7 +22,18 @@ for name in 'sin cos tan asin acos atan atan2'.split():
 
 #% Calculus
 
+def sqrt(x):
+    return x ** 0.5
+
 def differentiate(f, dx=0.0000001):
     def df(x):
         return (f(x+dx)-f(x))/dx
     return df
+
+def sign(x):
+    if x == 0:
+        return 0
+    elif x < 0:
+        return -1
+    else:
+        return +1
