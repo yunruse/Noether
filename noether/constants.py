7c2e9ce7c487f1da5e5df06e3803031833e4f019
Jack Neil Dobson <jackndobson@gmail.com>
2018-10-17 15:58:50 +0100

    Add naturalised units
    The sneakier eye may notice the loss of `m_p`: I'm moving particle constants to its own file. Being snarky, it uses clever stuff such as defining baryons simply with their quarks and mass, but it's not complete yet.

--- noether/constants.py
+++ noether/constants.py
@@ -7,13 +7,13 @@ circle = tau * Radian
 
 # Physical constants
 
-c = 299_792_458 * Metre / Second
-Lightsecond = c * Second
+c = Unit(299_792_458 * Metre / Second, 'c')
+Lightsecond = Unit(c * Second, 'ls')
 Lightyear = Unit(c * Year, 'ly')
 
-grav = 6.67408e-11 * Newton * Metre**2 / Kilogram**2
-h = 6.626_070_040e-34 * Joule * Second
-hbar = h / tau
+grav = Unit(6.67408e-11 * Newton * Metre**2 / Kilogram**2, 'G')
+h = Unit(6.626_070_040e-34 * Joule * Second, 'h')
+hbar = Unit(h / tau, 'ħ')
 
 mu_0 = 2 * tau * 1e-7 * Newton / Ampere ** 2
 e_0  = 1 / (mu_0 * c**2)
@@ -21,20 +21,23 @@ z_0  = mu_0 * c
 k_e  = 1 / (2 * tau * e_0)
 
 # Particle + chemical constants
-
-e = 1.60217646e-19 * Coulomb
-eV = electronvolt = e * Volt
-eVm = eV/c**2
-
-m_e = 9.109_383_56e-31 * Kilogram
-m_p = 1.672_621_898e-27 * Kilogram
+e = Unit(1.60217646e-19 * Coulomb, 'e')
+m_e = Unit(9.109_383_56e-31 * Kilogram, 'mₑ')
 
 r_e = (k_e * e**2) / (m_e * c**2)
 
 n_a = 6.022_140_857e23 / Mole
+
+# Naturalised units
+eV = Unit(e * Volt, 'eV')
+MeV = Unit(M*eV, 'MeV')
+MeVc = Unit(M * eV / c, 'MeVc⁻¹')
+MeVc2 = Unit(MeVc/c, 'MeVc⁻²')
 
 # Adopted values
 
 g = 9.80665
 atom = 101325
 t = 273.15
+
+natural_units = (c, e, MeV, MeVc, MeVc2, grav)
