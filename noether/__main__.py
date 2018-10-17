99eb417a6ed77ff1ec52024d52bc416f8a7d94a5
Jack Neil Dobson <jackndobson@gmail.com>
2018-10-17 16:24:25 +0100

    Allow module to load file
    I think there's a variable issue here.

--- noether/__main__.py
+++ noether/__main__.py
@@ -1,10 +1,23 @@
+import sys
+import os
+
 from . import physics
 from . import repl
+from .language import Noether
 
-def main():
+def main(argv):
     print('Noether\n')
+    
     ns = dict(physics.__dict__)
+    
+    if len(argv):
+        fname = os.path.join(os.getcwd(), argv[0])
+        if os.path.isfile(fname):
+            with open(fname, encoding='utf8') as f:
+                code = ''.join(f.readlines())
+                Noether.exec(code, globals=ns)
+    
     ns['__name__'] = '__main__'
     repl.repl(ns)
 
-main()
+main(sys.argv[1:])
