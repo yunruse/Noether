1ded6f249ae6a61367da87e603d49d4358018514
Jack Neil Dobson <jackndobson@gmail.com>
2018-10-18 10:20:21 +0100

    Add package import

---
+++ noether/__init__.py
@@ -0,0 +1,12 @@
+"""Noether: Physics calculator"""
+
+__author__    = "Mia yun Ruse"
+__copyright__ = "Copyright 2018 Mia yun Ruse"
+__status__    = "Prototype"
+__email__     = 's-noether@yunru.se'
+
+# The namespace is specified elsewhere,
+# as it is used as the default namespace
+# if scripts are ran via `python -m noether`.
+from .namespace import *
+
