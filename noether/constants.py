17857060d6d61c1c7ff9a98f213f3eecd5d1ac9f
Mia Dobson <mia@yunruse>
2019-03-26 23:11:59 +0000

    Update SI 2019 constants
    They're now fixed forever, so I won't have to trawl CODATA!
    Well, they are in May. I don't think this'll be done by then ahaha

--- noether/constants.py
+++ noether/constants.py
@@ -8,17 +8,21 @@ from .scale import prefix
 from .unit import Unit, BaseUnit, Dimension  # noqa: F401
 from .unitCatalogue import *
 
+# Trigonometry
+
 tau = pi * 2
 circle = tau * Radian
 
-# Physical constants
+# Quantum
+
+h = BaseUnit(6.626_070_15e-34 * Joule * Second, "h")
+hbar = BaseUnit(h / tau, "ħ")
+
+# Electromagnetic
 
 c = BaseUnit(299_792_458 * Metre / Second, "c")
 Lightsecond = BaseUnit(c * Second, "ls")
 Lightyear = BaseUnit(c * Year, "ly")
-
-h = BaseUnit(6.626_070_040e-34 * Joule * Second, "h")
-hbar = BaseUnit(h / tau, "ħ")
 
 Permeability = Force / Current ** 2
 mu_0 = BaseUnit(2 * tau * 1e-7 * Newton / Ampere ** 2)
@@ -29,23 +33,29 @@ e_0 = BaseUnit(1 / (mu_0 * c ** 2))
 Impedance = Permeability * Speed
 z_0 = BaseUnit(mu_0 * c)
 
-grav = BaseUnit(6.67408e-11 * Newton * Metre ** 2 / Kilogram ** 2)
+# Other fields
+grav = BaseUnit(6.674_08e-11 * Newton * Metre ** 2 / Kilogram ** 2)
 k_e = BaseUnit(1 / (2 * tau * e_0))
 
+# Material
 
-# Particle + chemical constants
-e = BaseUnit(1.602_176_46e-19 * Coulomb, "e")
+n_a = 6.022_140_76e23 / Mole
+
+k_B = BaseUnit(1.380649e-23 * Joule / Kelvin, 'k_b')
+R = n_a * k_B
+
+# Atomic
+
+e = BaseUnit(1.602_176_634e-19 * Coulomb, "e")
 m_e = BaseUnit(9.109_383_56e-31 * Kilogram, "mₑ")
 
 alpha = k_e * e ** 2 / (hbar * c)
 
 r_e = (k_e * e ** 2) / (m_e * c ** 2)
-
-n_a = 6.022_140_857e23 / Mole
-
 rydberg = (m_e * e ** 4) / (8 * e_0 ** 2 * h ** 3 * c)
 
 # Naturalised units
+
 eV = BaseUnit(e * Volt, "eV")
 MeV = BaseUnit(prefix.M * eV, "MeV")
 MeVc = BaseUnit(MeV / c, "MeVc⁻¹")
