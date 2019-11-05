02dcd09206ed48334b4f151d434ea133b6707f69
Mia Dobson <mia@yunru.se>
2019-11-05 11:57:15 +0000

    Add more unusual/humorous units

--- noether/unit/catalogue/unusual.py
+++ noether/unit/catalogue/unusual.py
@@ -1,16 +1,36 @@
 from .si import *
 from .conventional import *
-from .imperial *
+from .imperial import *
+from .scientific import *
+from .data import *
 
 hand = 4 * inch
 horse = 8 * foot
 
+potrzebie = metre * 2.263347539605392
+ngogn = 1000 * potrzebie**3
+smoot = metre * 1.67005
+sheppey = mile * 7/8
+
+new_york_second = planck_second = 3.39125e-44 * second
+
+friedman = year / 2
+warhol = U(minute * 15, SI=True)
+
+beard_second = angstrom * 100
+beard_inch = inch / (beard_second/second)
+
 football_pitch = 105 * 68 * metre**2
 wales = 20799e6 * metre**2
-
 dogyear = year / 7
+amazon_river = cumec * 216000
+sverdrup = U(cumec * 1e6, "Sv")
+banana_equivalent_dose = 78e-9 * gray
+
+mickey = pixel
+
 sol = 88775 * second
 galacticyear = 225 * 1e9 * year
 
 firkin = 90 * pound
-smoot = 1.67005 * metre
+bloit = mile * 2/3  # zork
