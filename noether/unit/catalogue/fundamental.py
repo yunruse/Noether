8db979647567ac11417c1d7bc896c555db3bf31f
Mia Dobson <mia@yunru.se>
2020-01-22 12:30:05 +0000

    Refactor fundamental units
    Weird transmogrification of things. Moved constants to top so they could be imported from any package

--- noether/unit/catalogue/fundamental.py
+++ noether/unit/catalogue/fundamental.py
@@ -2,6 +2,16 @@
 Fundamental units and unit defining helpers from which all other units derive.
 """
 
+BASE_UNITS = (
+    (1, "luminosity", "cd", True, "candela"),
+    (2, "current", "A", True, "amp", "ampere"),
+    (3, "temperature", "K", True, "kelvin"),
+    (4, "mass", "kg", False, "kilogram"),
+    (5, "substance", "mol", True, "mole"),
+    (6, "length", "m", True, "metre", "meter"),
+    (7, "time", "s", True, "second")
+)
+
 from ..measure import Unit, Measure, Dimension
 from ..scale import prefix_SI, prefix_IEC
 
@@ -19,28 +29,12 @@ def U(value, *symbols, display=None, SI=False, IEC=False):
 
 # SI units
 
-def base_units():
-    # yapf: disable
-    _BASE_UNITS = (
-        (1, "luminosity", "cd", True, "candela"),
-        (2, "current", "A", True, "amp", "ampere"),
-        (3, "temperature", "K", True, "kelvin"),
-        (4, "mass", "kg", False, "kilogram"),
-        (5, "substance", "mol", True, "mole"),
-        (6, "length", "m", True, "metre", "meter"),
-        (7, "time", "s", True, "second")
-    )
-    # yapf: enable
-
-    for u in _BASE_UNITS:
-        display_order, name, symbol, SI, *units = u
-        dim = Dimension.new(display_order, name, symbol)
-        globals()[name] = dim
-        unit = U(dim, symbol, SI=SI)
-        for name in units:
-            globals()[name] = unit
-
-base_units()
+for display_order, name, symbol, SI, *units in BASE_UNITS:
+    dim = Dimension.new(display_order, name, symbol)
+    globals()[name] = dim
+    unit = U(dim, symbol, SI=SI)
+    for name in units:
+        globals()[name] = unit
 
 dimensionless = Dimension()
 
