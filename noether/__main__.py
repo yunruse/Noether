876c980d27a39946c59b30eba647e51eb1a7b0e2
Mia Dobson <mia@yunru.se>
2020-02-03 14:15:57 +0000

    Add tentative interpreter

--- noether/__main__.py
+++ noether/__main__.py
@@ -1,5 +1,22 @@
-# Run Noether with `python -im` for interactive console.
+'''
+A quick pocket-calculator-like interpreter for Noether.
+
+Launches quickly by making heavy imports (numpy, matplotlib) in the background.
+'''
+
 import noether
-from . import *
+from noether import *
 
 display = Unit.display
+
+glob = dict(globals())
+loc = dict(locals())
+
+while True:
+    # todo: multi-line
+    # todo: lazy-load in shells
+    inp = input('>>> ')
+    try:
+        print(eval(inp, glob, loc))
+    except SyntaxError:
+        exec(inp, glob, loc)
