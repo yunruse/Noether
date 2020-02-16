435a3e2525b129bde189f054e84dc906eaa6e18f
Mia Dobson <mia@yunru.se>
2020-02-16 20:26:06 +0000

    Add real-life data units

--- noether/unit/catalogue/data.py
+++ noether/unit/catalogue/data.py
@@ -8,12 +8,17 @@ from .fundamental import (
 # data & IEC prefixes
 
 data = Dimension.new(3.5, "data", "D", "B")
+areal_density = data / length**2 
 
 byte = U(data, "B", SI=True, IEC=True)
 bit = shannon = U(byte / 8, "b", SI=True, IEC=True)
+
 nat = U(bit / log(2), 'nat', SI=True)
 trit = nat * log(3)
 hartley = ban = dit = U(nat * log(10), 'Hart')
+
+mutation_rate = 1 / data
+base_pair = bp = U(bit * 2, "bp", SI=True)
 
 data_rate = data / time
 bps = U(bit / second, 'bps', display=True, SI=True, IEC=True)
