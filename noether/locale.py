8d003c0c63acb43142710e4413eb8f31f3a9f013
Mia Dobson <mia@yunru.se>
2020-02-18 18:38:46 +0000

    Add abstract for locales

---
+++ noether/locale.py
@@ -0,0 +1,13 @@
+"""
+Noether locale and settings.
+
+Implements and handles:
+ - number formatting
+ - currency formatting
+ - default units
+"""
+
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
+# TODO: set 'conventional' UnitSystems based on location
