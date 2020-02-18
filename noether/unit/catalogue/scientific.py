5b076fdc6975f9897f10102625f94c92cb66a033
Mia Dobson <mia@yunru.se>
2020-02-18 18:50:02 +0000

    Shuffle catalogue.scientific, add Hubble
    A note: changes to the catalogue are not update-based but continuous

--- noether/unit/catalogue/scientific.py
+++ noether/unit/catalogue/scientific.py
@@ -8,24 +8,13 @@ from .fundamental import (
 from .si import watt, joule, pascal
 from .conventional import year
 
-angstrom = U(metre * 1e-10, "å")
-parsec = U(3.0857e16 * metre, "pc", SI=True)
-au = U(1.495_878_707e11 * metre, "au")
-
-are = U((10 * metre)**2, "a")
-barn = U(1e-28 * metre**2, "b")
-
 mps = metre / second
 c = U(299_792_458 * metre / second, "c")
-lightsecond = U(c * second, "ls", SI=True)
-lightyear = U(c * year, "ly", SI=True)
 
 g = 9.980665 * metre / second**2
 gee = g * kilogram
 
-solar_mass = U(1.98802e30 * kilogram, "msol")
-dalton = U(1.660_538_86e-27 * kilogram, "u")
-
+# human
 calorie = U(4.814 * joule, "cal", SI=True, display=False)
 kilocalorie = kcal = U(calorie * 1000, "kcal")
 
@@ -37,3 +26,24 @@ metre_mercury = meter_mercury = U(133_322.387_415 * pascal, "mhg", SI=True)
 bmi = U(kilogram / metre**2, "BMI")
 amagat = U(2.686_7811 / metre**3, 'Am', SI=True)
 met = U(58.2 * watt / metre ** 2, 'met')
+
+# small-scale
+angstrom = U(metre * 1e-10, "å")
+
+are = U((10 * metre)**2, "a")
+barn = U(1e-28 * metre**2, "b")
+
+dalton = U(1.660_538_86e-27 * kilogram, "u")
+
+# astronomical
+parsec = U(3.0857e16 * metre, "pc", SI=True)
+au = U(1.495_878_707e11 * metre, "au")
+
+lightsecond = U(c * second, "ls", SI=True)
+lightyear = U(c * year, "ly", SI=True)
+
+solar_mass = U(1.98802e30 * kilogram, "msol")
+# this one's quite controversial.
+# currently using Hubble Space Telescope.
+# Friedman et al, 2019. https://doi.org/10.3847/1538-4357/ab2f73
+hubble = (mps / parsec / 1000)(69.8, 1.9)
