d5669770c34dbe864e8cbfb14ba4e46f6759a830
Mia Dobson <mia@yunru.se>
2021-01-01 13:33:20 +0000

    Happy new year!

--- noether/__init__.py
+++ noether/__init__.py
@@ -15,9 +15,9 @@ not present, no matter how petty.
 """
 
 __author__ = "Mia yun Ruse"
-__copyright__ = "Copyright 2018-2020 Mia yun Ruse"
+__copyright__ = "Copyright 2018-2021 Mia yun Ruse"
 __status__ = "Alpha 0.1.3"
-__email__ = "s-noether@yunru.se"
+__email__ = "mia@yunru.se"
 
 from .conf import conf, conf_new
 
