dfafd87f5205e658228c0a4fd4e2ba4a876e1397
Mia yun Ruse <mia@yunru.se>
2021-08-26 04:27:15 +0100

    Show __main__ by example

--- noether/__main__.py
+++ noether/__main__.py
@@ -1,7 +1,18 @@
 # Run Noether with `python -im` for interactive console.
 import noether
+from noether.conf import conf_exists
 from . import *
 noe = noether
+
+if not conf_exists:
+    print('\n')
+del conf_exists
+
+print('''\
+>>> import noether
+>>> from noether import *
+>>> noe = noether
+''')
 
 # TODO: if given text, output what it evaluates to
 # TODO: set up syntax so, eg `4kg` -> `kg(4)`
