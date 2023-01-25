fd8dd31759c745ecfdb284edcdc224444955f089
Mia yun Ruse <mia@yunru.se>
2023-01-25 13:06:56 +0000

    add catalogue.toml
    It's waaay nicer to read!

--- tests/auto_catalogue.py
+++ tests/auto_catalogue.py
@@ -2,6 +2,7 @@
 Not a test -- just a quick catalogue generator.
 '''
 import json
+import toml
 from unittest import TestCase
 
 from noether import catalogue
@@ -13,3 +14,7 @@ class test_unit_display(TestCase):
         cat['$schema'] = './catalogue.schema.json'
         with open('noether/catalogue/catalogue.json', 'w') as f:
             json.dump(cat, f, indent=1)
+
+        del cat['$schema']
+        with open('noether/catalogue/catalogue.toml', 'w') as f:
+            toml.dump(cat, f)
