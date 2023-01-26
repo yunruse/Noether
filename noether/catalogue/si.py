be53fa5408274dcb69b7776b2c5e8240d0ea7f02
Mia yun Ruse <mia@yunru.se>
2023-01-26 16:26:42 +0000

    add SI unit symbols

--- noether/catalogue/si.py
+++ noether/catalogue/si.py
@@ -5,38 +5,38 @@ from ..core.DisplaySet import display as I
 from ..core.fundamental import *
 from .dimensions import *
 
-becquerel = I(Unit(frequency, 'becquerel', "Bq", SI))
-hertz = I(Unit(frequency, 'hertz', "Hz", SI))
+becquerel = Bq = Unit(frequency, 'becquerel', "Bq", SI)
+hertz = Hz = I(Unit(frequency, 'hertz', "Hz", SI))
 
-rad = radian = I(Unit(angle, 'radian', "rad", SI_small))
-sterad = steradian = I(Unit(solid_angle, 'steradian', "sterad", SI))
+radian = rad = I(Unit(angle, 'radian', "rad", SI_small))
+steradian = seterad = I(Unit(solid_angle, 'steradian', "sterad", SI))
 
 # % Dynamics
 
-newton = I(Unit(force, 'newton', "N", SI))
-pascal = I(Unit(pressure, 'pascal', "Pa", SI))
-joule = I(Unit(energy, 'joule', "J", SI))
-watt = I(Unit(power, 'watt', "W", SI))
+newton = N = I(Unit(force, 'newton', "N", SI))
+pascal = Pa = I(Unit(pressure, 'pascal', "Pa", SI))
+joule = J = I(Unit(energy, 'joule', "J", SI))
+watt = W = I(Unit(power, 'watt', "W", SI))
 
 # % Electromagnetism
 
-coulomb = I(Unit(charge, 'coulomb', "C", SI))
-volt = I(Unit(voltage, 'volt', "V", SI))
-farad = I(Unit(capacitance, 'farad', "F", SI))
-ohm = I(Unit(resistance, 'ohm', "Ω", SI))
-siemens = I(Unit(conductance, 'siemens', "S", SI))
-henry = I(Unit(inductance, 'henry', "H", SI))
-weber = I(Unit(magnetic_flux, 'weber', "Wb", SI))
-tesla = I(Unit(magnetic_flux_density, 'tesla', "T", SI))
+coulomb = C = I(Unit(charge, 'coulomb', "C", SI))
+volt = V = I(Unit(voltage, 'volt', "V", SI))
+farad = F = I(Unit(capacitance, 'farad', "F", SI))
+ohm = Ω = I(Unit(resistance, 'ohm', "Ω", SI))
+siemens = S = I(Unit(conductance, 'siemens', "S", SI))
+henry = H = I(Unit(inductance, 'henry', "H", SI))
+weber = Wb = I(Unit(magnetic_flux, 'weber', "Wb", SI))
+tesla = T = I(Unit(magnetic_flux_density, 'tesla', "T", SI))
 
 # % Radiation
 
-lumen = I(Unit(luminous_flux, 'lumen', "lum", SI))
-lux = I(Unit(illuminance, 'lux', "Lx", SI))
-gray = I(Unit(dose, 'gray', "Gy", SI))
-sievert = I(Unit(dose, 'sievert', "Sv", SI))
+lumen = lum = I(Unit(luminous_flux, 'lumen', "lum", SI))
+lux = Lx = I(Unit(illuminance, 'lux', "Lx", SI))
+gray = Gy = I(Unit(dose, 'gray', "Gy", SI))
+sievert = Sv = I(Unit(dose, 'sievert', "Sv", SI))
 
 # % Material properties
 
 cumec = I(Unit(flow, 'cumec', None, SI))
-katal = I(Unit(catalytic_activity, 'katal', "kat", SI))
+katal = kat = I(Unit(catalytic_activity, 'katal', "kat", SI))
