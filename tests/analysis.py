16a64acf56b1c80769a6818810a4d89a1969e817
Mia yun Ruse <mia@yunru.se>
2023-01-28 23:59:44 +0000

    move analysis to tests/analysis; ignore
    This includes `catalogue.json` &c. I don't want to version-micromanage them; they're best included in release downloads, IMO.

--- tests/analysis.py
+++ tests/analysis.py
@@ -1,5 +1,7 @@
 '''
-Quick automation tools that are useful to have done during testing.
+Quick analysis tools that are useful for development.
+
+Requires matplotlib.
 '''
 import csv
 import json
@@ -10,27 +12,33 @@ from unittest import TestCase
 
 from noether import catalogue, Config
 
-TEST_PATH = Path(__file__).parent
+PATH = Path(__file__).parent / 'analysis'
 
 
 class automation(TestCase):
     def test_export_config(self):
-        Config().save(TEST_PATH / 'default.conf')
+        Config().save(PATH / 'default.conf')
 
     def test_export_catalogue(self):
+        with open(PATH / 'catalogue.tsv', 'w') as f:
+            units = set(catalogue.units.values())
+            for u in sorted(units, key=lambda u: u.name.lower()):
+                chunks = [u.name, *(a for a, c in u._info())]
+                print('\t'.join(chunks), file=f)
+
         cat = catalogue.__json__()
-        cat['$schema'] = './catalogue.schema.json'
-        with open(TEST_PATH / 'catalogue.json', 'w') as f:
+        cat['$schema'] = '../catalogue.schema.json'
+        with open(PATH / 'catalogue.json', 'w') as f:
             json.dump(cat, f, indent=1)
 
         del cat['$schema']
-        with open(TEST_PATH / 'catalogue.toml', 'w') as f:
+        with open(PATH / 'catalogue.toml', 'w') as f:
             toml.dump(cat, f)
 
     def test_count_units(self, graph=True):
         FMT = '%Y-%m-%d'
 
-        with open('catalogue_count.csv') as f:
+        with open(PATH / 'catalogue_count.csv') as f:
             data = [(date, int(N)) for date, N in csv.reader(f)]
 
         now = datetime.now().strftime(FMT)
@@ -40,7 +48,7 @@ class automation(TestCase):
         else:
             data.append([now, N])
 
-        with open('catalogue_count.csv', 'w') as f:
+        with open(PATH / 'catalogue_count.csv', 'w') as f:
             for date, N in data:
                 f.write(f'{date},{N}\n')
 
@@ -55,11 +63,11 @@ class automation(TestCase):
         pyplot.title('Catalogue count')
 
         fig, ax = pyplot.gcf(), pyplot.gca()
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H:%M'))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
         pyplot.xticks(rotation=90)
         fig.tight_layout()
 
-        pyplot.savefig('catalogue_count.png')
+        pyplot.savefig(PATH / 'catalogue_count.png')
 
 
 if __name__ == '__main__':
