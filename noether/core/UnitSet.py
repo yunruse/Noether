e15c2555d8feefa18f8ad3de5fa3582638c5dd21
Mia yun Ruse <mia@yunru.se>
2023-02-12 11:43:40 +0000

    make UnitSet a set[Unit]

--- noether/core/UnitSet.py
+++ noether/core/UnitSet.py
@@ -11,22 +11,19 @@ from .Unit import Unit
 T = TypeVar('T', Unit, Dimension, 'UnitSet')
 
 
-class UnitSet:
+class UnitSet(set[Unit]):
     units: dict[Dimension, list[Unit]]
     dimension_names: dict[Dimension, list[str]]
 
     dimension_symbol: dict[str, str]
 
     def __init__(self, *items: Unit):
+        super().__init__()
+
         self.units = dict()
         self.dimension_names = dict()
         self.dimension_symbol = dict(dimensionless='')
         self.register(*items)
-
-    def __repr__(self):
-        return 'UnitSet({})'.format(', '.join(
-            str(i[-1]) for i in self.units.values()
-        ))
 
     def add(self, value: T, *names: str) -> T:
         if isinstance(value, Dimension):
@@ -34,6 +31,7 @@ class UnitSet:
             for n in names:
                 self.dimension_names[value].append(n)
         elif isinstance(value, Unit):
+            super().add(value)
             self.units.setdefault(value.dim, [])
             self.units[value.dim].append(value)
 
@@ -59,12 +57,15 @@ class UnitSet:
                 if n in self.dimension_names[value]:
                     self.dimension_names[value].remove(n)
         elif isinstance(value, Unit):
+            super().remove(value)
             self.units.setdefault(value.dim, [])
             if value in self.units[value.dim]:
                 self.units[value.dim].remove(value)
         elif isinstance(value, UnitSet):
             for units in value.units.values():
                 self.unregister(*units)
+
+    # helper functions
 
     def register(self, *units: Unit):
         for unit in units:
