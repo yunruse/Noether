19ed6a39021bf57d42def93cf447ac1faf2ef43e
Mia yun Ruse <mia@yunru.se>
2023-02-12 12:01:47 +0000

    Reduce UnitSet to bare minimum

--- noether/core/UnitSet.py
+++ noether/core/UnitSet.py
@@ -4,73 +4,24 @@ List of units used for display purposes.
 Also handles dimension names.
 '''
 
-from typing import TypeVar
-from .Dimension import Dimension
 from .Unit import Unit
 
 
 class UnitSet(set[Unit]):
-    units: dict[Dimension, list[Unit]]
-    dimension_names: dict[Dimension, list[str]]
+    '''
+    set[Unit] with convenience functions.
+    '''
 
-    dimension_symbol: dict[str, str]
-
-    def __init__(self, *items: Unit):
+    def __init__(self, *iterable: 'Unit | UnitSet'):
         super().__init__()
+        for x in iterable:
+            if isinstance(x, Unit):
+                self.add(x)
+            else:
+                self |= x
 
-        self.units = dict()
-        self.dimension_names = dict()
-        self.dimension_symbol = dict(dimensionless='')
-        self.register(*items)
-
-    T = TypeVar('T', Unit, Dimension, 'UnitSet')
-
-    def add(self, value: T, *names: str) -> T:
-        if isinstance(value, Dimension):
-            self.dimension_names.setdefault(value, [])
-            for n in names:
-                self.dimension_names[value].append(n)
-        elif isinstance(value, Unit):
-            super().add(value)
-            self.units.setdefault(value.dim, [])
-            self.units[value.dim].append(value)
-
-            if value.symbols:
-                for n in self.dimension_names.get(value.dim, []):
-                    self.dimension_symbol[n] = value.symbol
-        elif isinstance(value, UnitSet):
-            for units in value.units.values():
-                self.add(units[-1])
-
+    def add(self, value: Unit) -> Unit:
+        super().add(value)
         return value
 
     __call__ = add
-
-    def remove(
-        self,
-        value: 'Unit | Dimension | UnitSet',
-        *names: str
-    ):
-        if isinstance(value, Dimension):
-            self.dimension_names.setdefault(value, [])
-            for n in names:
-                if n in self.dimension_names[value]:
-                    self.dimension_names[value].remove(n)
-        elif isinstance(value, Unit):
-            super().remove(value)
-            self.units.setdefault(value.dim, [])
-            if value in self.units[value.dim]:
-                self.units[value.dim].remove(value)
-        elif isinstance(value, UnitSet):
-            for units in value.units.values():
-                self.unregister(*units)
-
-    # helper functions
-
-    def register(self, *units: Unit):
-        for unit in units:
-            self.add(unit)
-
-    def unregister(self, *units: Unit):
-        for unit in units:
-            self.remove(unit)
--- noether/core/_DisplayHandler.py
+++ noether/core/_DisplayHandler.py
@@ -1,9 +1,81 @@
 
+from typing import TypeVar
+from .Unit import Unit
 from .UnitSet import UnitSet
+from .Dimension import Dimension
 
 
 class DisplayHandler(UnitSet):
-    pass
+    '''
+    Internal class used to provide names and units
+    to various dimensions.
+    '''
+
+    units: dict[Dimension, list[Unit]]
+    dimension_names: dict[Dimension, list[str]]
+
+    dimension_symbol: dict[str, str]
+
+    def __init__(self, *items: Unit):
+
+        self.units = dict()
+        self.dimension_names = dict()
+        self.dimension_symbol = dict(dimensionless='')
+        
+        super().__init__()
+        self.register(*items)
+
+    T = TypeVar('T', Unit, Dimension, 'UnitSet')
+
+    def add(self, value: T, *names: str) -> T:
+        if isinstance(value, Dimension):
+            self.dimension_names.setdefault(value, [])
+            for n in names:
+                self.dimension_names[value].append(n)
+        elif isinstance(value, Unit):
+            super().add(value)
+            self.units.setdefault(value.dim, [])
+            self.units[value.dim].append(value)
+
+            if value.symbols:
+                for n in self.dimension_names.get(value.dim, []):
+                    self.dimension_symbol[n] = value.symbol
+        elif isinstance(value, UnitSet):
+            for units in value.units.values():
+                self.add(units[-1])
+
+        return value
+
+    __call__ = add
+
+    def remove(
+        self,
+        value: 'Unit | Dimension | UnitSet',
+        *names: str
+    ):
+        if isinstance(value, Dimension):
+            self.dimension_names.setdefault(value, [])
+            for n in names:
+                if n in self.dimension_names[value]:
+                    self.dimension_names[value].remove(n)
+        elif isinstance(value, Unit):
+            super().remove(value)
+            self.units.setdefault(value.dim, [])
+            if value in self.units[value.dim]:
+                self.units[value.dim].remove(value)
+        elif isinstance(value, UnitSet):
+            for units in value.units.values():
+                self.unregister(*units)
+
+    # helper functions
+
+    def register(self, *units: Unit):
+        for unit in units:
+            self.add(unit)
+
+    def unregister(self, *units: Unit):
+        for unit in units:
+            self.remove(unit)
 
 
 display = DisplayHandler()
