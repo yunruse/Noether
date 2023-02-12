f7443069eb6bcac6b41cab7926929d85f6a0fc3e
Mia yun Ruse <mia@yunru.se>
2023-02-12 23:12:21 +0000

    add various more fictional units
    I am honour-bound to add a unit whenever I encounter one (otherwise I'll forget it).
    And I SO INNOCENTLY thought a "solar sweep" was just, like, not a tumblr user trying to fucking deliver psychic damage ._.

--- noether/catalogue/humorous/fictional.py
+++ noether/catalogue/humorous/fictional.py
@@ -1,10 +1,13 @@
 '''
-Fictional units.
+A variety of fictional units.
 Only distinctly defined units are provided.
+
+Sourced from various fan wikis and TV Tropes.
 '''
 
 from noether.core import Unit, UnitSet
-from ..conventional import day
+from noether.core.fundamental import second
+from ..conventional import day, year, minute
 from ..conventional import inch
 
 # % Tolkien (taken from appendices)
@@ -35,3 +38,33 @@ lar = lár = daur = Unit(
     " said to be 5000 paces, after which marchers would briefly halt.")
 
 TOLKIEN = UnitSet(yén & enquië & ré, lár & ranga)
+
+
+# % Homestuck
+solar_sweep = alternian_year = Unit(
+    year * 13/6, info="i h8 that i felt obliged to add this unit ::(")
+
+
+# % Dragonball
+takk = Unit(minute * 0.48, "takk")
+
+# % Superman
+thrib = Unit(second, "thrib")
+dendar = Unit(thrib * 100, "dendar")
+wolu = Unit(thrib * 10_000, "wolu")
+zetyar = Unit(wolu * 10, "zetyar")
+lorax = Unit(zetyar * 73, "lorax")
+kryptonian_year = Unit(lorax * 6, "kryptonian_year")
+
+chronon = knorrian_hour = Unit(minute * 40, ["chronon", "knorrian_hour"])
+
+# % Transformers
+breem = Unit(minute * 8.3, "breem")
+vorn = Unit(year * 83, "vorn")
+astrosecond = Unit(breem / 1000, "astrosecond")
+# TODO: https://tfwiki.net/wiki/Category:Measurements
+
+
+# TODO: Like, every other fictional unit
+# https://tvtropes.org/pmwiki/pmwiki.php/Main/Microts
+# https://tvtropes.org/pmwiki/pmwiki.php/Main/FantasticMeasurementSystem
