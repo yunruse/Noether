f113a190fd67b48c3993b1616b8e496b85929a20
Mia yun Ruse <mia@yunru.se>
2023-07-02 05:46:53 +0100

    Improve __main__.py substantially

--- noether/__main__.py
+++ noether/__main__.py
@@ -1,3 +1,7 @@
+"""
+TODO: write a description here...
+"""
+
 import noether
 from noether import *
 
@@ -6,41 +10,61 @@ from noether import *
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument(
-    '--no-color',
+    '--no-color',  # TODO: what is the gnu standard here?
     action='store_false',
-    dest='color')  # TODO: what is the gnu standard here?
+    help='Suppress colour output',
+    dest='color')
+parser.add_argument(
+    '--value', '-V',  # TODO: what is the gnu standard here?
+    action='store_true',
+    help='Display the value only (equivalent to str(statement))')
+
+# TODO: add --si
+
 parser.add_argument('terms', nargs='*')
 
 args = parser.parse_args()
 
-if args.terms:
-    # this is very basic at the moment!
-    __terms = " ".join(args.terms)
-    del args, parser, ArgumentParser
-    print(eval(__terms))
-
-    # regular exit(0) does not work with `python -im noether` - it displays an exception and continues.
-    # _exit, meanwhile, forces an exit.
-    # Note that this will NOT process any __del__ cleanups,
-    # but this is considered not a major problem – bare evals automatically self-delete this way,
-    # and we can't assign variables in eval(), so we shouldn't feasibly be able to, for example,
-    # have `open()` leave a file handle open or something.
-    import os
-    os._exit(0)
-
-print(f'{len(catalogue.units())} units, {len(list(catalogue.prefixes()))} prefixes')
-print('''
->>> import noether
->>> from noether import *''')
-
-if args.color:
+pretty = None
+if args.color and not args.value:
     try:
         from rich import pretty, print
     except ImportError:
         pass
     else:
         pretty.install()
-        print('''\
+
+if args.terms:
+    # this is very basic at the moment!
+    import os
+    try:
+        value = eval(" ".join(args.terms))
+        if args.value:
+            print(value)
+        else:
+            print(repr(value))
+    except Exception as e:
+        import traceback
+        traceback.print_exception(e, limit=0)
+        import os
+        os._exit(2)
+    else:
+        import os
+        os._exit(0)
+    # regular exit(0) does not work with `python -im noether` - it displays an exception and continues.
+    # _exit, meanwhile, forces an exit.
+    # Note that this will NOT process any __del__ cleanups,
+    # but this is considered not a major problem – bare evals automatically self-delete this way,
+    # and we can't assign variables in eval(), so we shouldn't feasibly be able to, for example,
+    # have `open()` leave a file handle open or something.
+
+print(f'{len(catalogue.units())} units, {len(list(catalogue.prefixes()))} prefixes')
+print('''
+>>> import noether
+>>> from noether import *''')
+
+if pretty is not None:
+    print('''\
 >>> from rich import pretty, print
 >>> pretty.install()''')
 print()
